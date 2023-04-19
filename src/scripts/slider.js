104eea68e4ff1ecbcf8b4e139998a3d07830132b
Nazar <nazar.poplavsky@gmail.com>
2023-04-19 15:08:46 +0200

    Hero styling fixed

---
+++ src/scripts/slider.js
@@ -0,0 +1,17 @@
+import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+	pagination: {
+		el: ".swiper-pagination",
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	speed: 400,
+	modules: [Navigation, Pagination],
+});
