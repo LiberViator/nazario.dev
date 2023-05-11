cf9c1ec6cb4d3adb7cd5c511e2c98020e49bd432
Nazar <nazar.poplavsky@gmail.com>
2023-05-11 15:51:57 +0200

    Implemented Swiper carousel

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,17 +1,22 @@
 import Swiper, { Navigation, Pagination } from "swiper";
-// import Swiper and modules styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 
 const swiper = new Swiper(".swiper", {
 	pagination: {
 		el: ".swiper-pagination",
+		type: "fraction",
 	},
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
+	slidesPerView: 1,
+	spaceBetween: 24,
 	speed: 400,
+	breakpoints: {
+		480: {
+			slidesPerView: 2,
+			spaceBetween: 32,
+		},
+	},
 	modules: [Navigation, Pagination],
 });
