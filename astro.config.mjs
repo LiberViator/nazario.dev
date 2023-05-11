897d910dc4287791ea9387de94a76f7190094e9c
Nazar <nazar.poplavsky@gmail.com>
2023-05-11 14:04:50 +0200

    Astro & TailwindCSS were updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import alpinejs from "@astrojs/alpinejs";
-
-// https://astro.build/config
 import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://nazario.dev/",
-  integrations: [tailwind(), alpinejs(), compress()],
+	site: "https://nazario.dev/",
+	integrations: [tailwind(), alpinejs(), compress()],
 });
