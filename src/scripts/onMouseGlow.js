b29f4709216adb9c9b1fa23db2863b95aeb8abbb
Nazar <nazar.poplavsky@gmail.com>
2023-05-18 15:31:38 +0200

    Small styling fixes

--- src/scripts/onMouseGlow.js
+++ src/scripts/onMouseGlow.js
@@ -1,13 +1,10 @@
-const handleOnMouseMove = (e) => {
-	const { currentTarget: target } = e;
-	const rect = target.getBoundingClientRect(),
-		x = e.clientX - rect.left,
-		y = e.clientY - rect.top;
+document.getElementById("body").onmousemove = (e) => {
+	for (const card of document.getElementsByClassName("border-shine")) {
+		const rect = card.getBoundingClientRect(),
+			x = e.clientX - rect.left,
+			y = e.clientY - rect.top;
 
-	target.style.setProperty("--mouse-x", `${x}px`);
-	target.style.setProperty("--mouse-y", `${y}px`);
+		card.style.setProperty("--mouse-x", `${x}px`);
+		card.style.setProperty("--mouse-y", `${y}px`);
+	}
 };
-
-for (const card of document.querySelectorAll(".border-shine")) {
-	card.onmousemove = (e) => handleOnMouseMove(e);
-}
