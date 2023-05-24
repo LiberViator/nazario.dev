ab582cce3ebb695e083390f8b254171e81c09c44
Nazar <nazar.poplavsky@gmail.com>
2023-05-24 13:36:40 +0200

    Hero height fix

--- src/layouts/Hero.svelte
+++ src/layouts/Hero.svelte
@@ -1,13 +1,12 @@
 <script>
 	import NavLink from "@components/Nav.svelte";
 	export let navLinks, socialLinks;
-
 </script>
 
 <section
 	id="hero"
-	class="paddings relative mx-auto grid max-h-[960px] min-h-[640px] w-full max-w-6xl grid-cols-1 justify-center gap-8 lg:grid-cols-2"
-	style={`height:${window?.innerHeight}px`}
+	class="paddings relative mx-auto grid max-h-[960px] h-[100svh] min-h-[640px] w-full max-w-6xl grid-cols-1 justify-center gap-8 lg:grid-cols-2"
+	style={`height:${window.innerHeight}px`}
 >
 	<div class="absolute inset-0 overflow-hidden lg:relative lg:overflow-visible">
 		<img
