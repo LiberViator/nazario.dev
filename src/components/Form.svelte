e81da7f73b1755f1b560251bd8ce96d40b8e0f5f
Nazar <nazar.poplavsky@gmail.com>
2023-06-01 10:49:09 +0200

    Form Captcha added

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -1,17 +1,19 @@
-<script>
-	const access_key = "f21c06ae-2d7f-43b7-be97-c94e2cd54eb8";
-</script>
-
+<svelte:head>
+	<!-- Captcha -->
+	<script src="https://www.google.com/recaptcha/api.js"></script>
+	<script>
+		function onSubmit(token) {
+			document.getElementById("contact-form").submit();
+		}
+	</script>
+</svelte:head>
 <form
 	id="contact-form"
-	action="https://api.web3forms.com/submit"
+	action="mailto:recipient@example.com"
 	method="POST"
 	class="max-md:paddings intersect-once border-shine relative mx-auto flex w-full flex-col rounded-t-2xl bg-slate-700 bg-form-gradient pb-16 pt-3 opacity-5 shadow-form intersect:animate-form sm:max-w-[560px] md:px-8"
 >
-	<input type="hidden" name="access_key" value={access_key} />
-
 	<span class="mb-6 flex h-1 w-[min(24%,128px)] self-center rounded bg-slate-600" />
-
 	<fieldset class="flex w-full flex-wrap gap-x-4">
 		<div class="flex w-full flex-[1_1_200px] flex-col">
 			<label for="user_name" class="mb-3 w-fit text-base text-slate-400">Imię:</label>
@@ -21,11 +23,10 @@
 				type="text"
 				minlength="3"
 				class="mb-4 h-12 w-full rounded-xl border border-transparent bg-slate-600 px-6 py-3 text-base transition-colors hover:border-slate-500"
-				placeholder="Jan"
+				placeholder="Artur"
 				required
 			/>
 		</div>
-
 		<div class="flex w-full flex-[1_1_200px] flex-col">
 			<label for="user_number" class="mb-3 w-fit text-base text-slate-400">Numer: (Opcj.)</label>
 			<input
@@ -38,7 +39,6 @@
 			/>
 		</div>
 	</fieldset>
-
 	<label for="user_email" class="mb-3 w-fit text-base text-slate-400">E-Mail:</label>
 	<input
 		id="user_email"
@@ -48,7 +48,6 @@
 		placeholder="mój@gmail.com"
 		required
 	/>
-
 	<label for="user_message" class="mb-3 w-fit text-base text-slate-400">Wiadomość:</label>
 	<textarea
 		id="user_message"
@@ -57,9 +56,12 @@
 		placeholder="Cześć, Nazar! Mam swój barbershop w Krakowie i potrzebuję strony internetowej, która zachęci nowych klientów do skorzystania z moich usług. Chcę, aby była atrakcyjna i zawierała wszystkie niezbędne informacje."
 		required
 	/>
-	<input type="hidden" name="redirect" value="https://web3forms.com/success" />
-
-	<button type="submit" class="cta_button w-full self-end bg-yellow-300 text-black hover:shadow-glow-yellow sm:w-64"
+	<button
+		type="submit"
+		data-sitekey="6LdLKlomAAAAACoPQt1N0kcGeWvx_XW2iR78IftF"
+		data-callback="onSubmit"
+		data-action="submit"
+		class="g-recaptcha cta_button w-full self-end bg-yellow-300 text-black hover:shadow-glow-yellow sm:w-64"
 		>Wyślij wiadomość</button
 	>
 </form>
